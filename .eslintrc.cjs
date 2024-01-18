7e6e273e20a6c9eabc3e139c4c316370d2f7bfac
arsheelsheikh <sheikharsheel@gmail.com>
2024-01-19 04:47:28 +0530

    update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,16 +3,22 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked', // Use type-aware lint rules
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended', // Include recommended React rules
+    'plugin:react/jsx-runtime',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
   },
-}
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    // Additional rules or overrides can be added here
+  },
+};
